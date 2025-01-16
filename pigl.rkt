1a7e9eb4c9e705c5c757c002f8dcdba6f15a6280
Bluebaritone21 <82056947+Bluebaritone21@users.noreply.github.com>
2025-01-16 22:19:20 +0000

    Fixed a bug

--- pigl.rkt
+++ pigl.rkt
@@ -2,7 +2,7 @@
 (require (planet dyoo/simply-scheme:2:2))
 (require xrepl)
 (define (pigl wd)
-        (if (member? (first wd) '(a e i o u))
+        (if (not (member? (first wd) '(a e i o u)))
             (pigl (word (bf wd) (first wd)))
             (word wd 'ay))
 )
